11c0dbe2c17e8762907f94b8e9f1df560b4cf90d
PeyaPeyaPeyang <peyang@peya.tokyo>
2025-12-21 02:33:08 +0900

    Remove maven publishing configuration and related tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,8 @@
-import com.vanniktech.maven.publish.tasks.JavadocJar
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 plugins {
     java
-    id("com.vanniktech.maven.publish") version "0.35.0"
 }
 
 group = "io.github.togar2"
@@ -32,40 +30,3 @@ tasks.test {
     onlyIf { false }
 }
 
-mavenPublishing {
-    publishToMavenCentral(automaticRelease = true, validateDeployment = false)
-    signAllPublications()
-
-    val mcVersion = libs.versions.minestom.get().split("-")[1]
-    val date = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy.MM.dd"))
-    val version = "$date-$mcVersion"
-    coordinates(project.group.toString(), project.name, version)
-
-    pom {
-        name = project.name
-        description = project.description
-        url = "https://github.com/TogAr2/MinestomPvP/"
-        licenses {
-            license {
-                name = "The Apache License, Version 2.0"
-                url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
-            }
-        }
-        developers {
-            developer {
-                id = "TogAr2"
-                name = "TogAr2"
-                url = "https://github.com/TogAr2/"
-            }
-        }
-        scm {
-            url = "https://github.com/TogAr2/MinestomPvP/"
-            connection = "scm:git:git://github.com/TogAr2/MinestomPvP.git"
-            developerConnection = "scm:git:ssh://git@github.com/TogAr2/MinestomPvP.git"
-        }
-    }
-}
-
-tasks.withType<GenerateModuleMetadata>().configureEach {
-    dependsOn(tasks.withType<JavadocJar>())
-}
